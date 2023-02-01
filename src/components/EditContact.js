995d2136674c5149c7906707432a06399bc108ff
Ahmed Raihan <imraihan00@gmail.com>
2023-02-02 04:42:10 +0600

    Update EditContact.js

--- src/components/EditContact.js
+++ src/components/EditContact.js
@@ -5,7 +5,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 const EditContact = () => {
 const { id } = useParams();
-
+// console.log(id);
   const [editData, setEditData] = useState([]);
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
@@ -27,8 +27,8 @@ const { id } = useParams();
       const editDate = dateFormat();
       let contact = {id : id,name,phone,company,address,createDate: editData.createDate,editDate}
       const prevData = localStorage.getItem('contacts') && localStorage.getItem('contacts').length > 0 ? JSON.parse(localStorage.getItem('contacts')) : []
-      const filterOut = prevData.filter((contact) => contact.contact.id !== id);
-      localStorage.setItem('contacts', JSON.stringify([...filterOut, {contact}]))
+      prevData.filter((contact) => contact.contact.id === id)[0].contact = contact;
+      localStorage.setItem('contacts', JSON.stringify(prevData))
       navigate('/');
     }
     const handleChange = (e) => {
@@ -48,19 +48,19 @@ const { id } = useParams();
         <form onSubmit={handleContactEdit}>
         <div className='form-group'>
         <label>Name</label>
-          <input id="name" type="text" className='form-control' name="name" value={name} onChange={handleChange}></input>
+          <input id="name" type="text" required className='form-control' name="name" value={name} onChange={handleChange}></input>
         </div>
         <div className='form-group'>
         <label>Phone</label>
-          <input id="phone" type="phone" className='form-control' name="phone" value={phone} onChange={handleChange}></input>
+          <input id="phone" type="phone" required className='form-control' name="phone" value={phone} onChange={handleChange}></input>
         </div>
         <div className='form-group'>
         <label>Company</label>
-        <input id="company" type="text" className='form-control' name="company" value={company} onChange={handleChange}></input>
+        <input id="company" type="text" required className='form-control' name="company" value={company} onChange={handleChange}></input>
         </div>
         <div className='form-group'>
         <label>Address</label>
-        <input id="address" type="text" className='form-control' name="address" value={address} onChange={handleChange}></input>
+        <input id="address" type="text" required className='form-control' name="address" value={address} onChange={handleChange}></input>
         </div> <br></br>
         <button type='submit' className='btn btn-success btn-md'>Update Contact</button>
         <button className='btn' onClick={() => navigate(-1)}>Back</button>
